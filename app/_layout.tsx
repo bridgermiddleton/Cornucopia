7653e130f724e0b1fbd770e991180424052b22dc
Bridger Middleton <bridgermiddleton@Bridgers-MacBook-Pro.local>
2025-02-11 22:33:28 -0500

    made rough draft of home page and some tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout() {
   }, [user, initializing]);
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{headerShown: false}} />
       <Stack.Screen name="(auth)" options={{headerShown: false}} />
     </Stack>
   )
